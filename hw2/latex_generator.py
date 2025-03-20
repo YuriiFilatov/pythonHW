b3759ee665ee52c5719284e7abb476e19f92f8df
Yurii Filatov <yura_filatov_2004@mail.ru>
2025-03-20 20:27:43 +0000

    fixes

--- hw2/latex_generator.py
+++ hw2/latex_generator.py
@@ -32,28 +32,3 @@ def generate_latex_picture(image_path, width, caption=None, label=None):
     figure_code.append(r"\end{figure}")
 
     return "\n".join(figure_code)
-
-
-def main():
-    table = [[[1, 2, 3], [3, 2, 1], [2, 3, 1], [2, 1, 3], [1, 3, 2], [3, 1, 2]]]
-    table_tex = generate_latex_table(table)
-
-    image_tex = generate_latex_picture("pictures/picture.png", 0.5, "*** Врагам Кубани!!!")
-
-    latex_document = r"""\documentclass{article}
-    \usepackage[T1]{fontenc}
-    \usepackage[utf8]{inputenc}
-    \usepackage[russian]{babel}
-    \usepackage{graphicx}
-    \begin{document}
-    """ + table_tex + r"""
-    \bigskip
-    """ + image_tex + r"""
-    \end{document} 
-    """
-
-    with open("result.tex", "w+", encoding="utf-8") as f:
-        f.write(latex_document)
-
-if __name__ == "__main__":
-    main()
